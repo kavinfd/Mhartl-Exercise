cd449372abde70d899d223bc65539d7b5f334569
kavinfd <kavin@freshdesk.com>
2014-07-03 09:51:20 +0530

    Initial commit
    bare rails environment
    
    Signed-off-by: kavinfd <kavin@freshdesk.com>

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '1006bedbe195dbce8a6144936b30d0c0e075d53d1181fed0f761ab06b38a221ff92d125a0825f923d87703ae79e573baea860218c45299c3b0cec1a4d9359662';
